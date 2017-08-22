25d05d8d1566e22138982af1d194d3191e8b777f
druane10 <druane32@yahoo.com>
2017-08-21 19:17:10 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   get 'posts/sports'
 
+  get '/posts/:id', to: 'posts#show', as: 'post'
+
   root 'posts#all'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
