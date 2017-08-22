d72b5ec3d6baeb33587d88d97ad3da00d054018c
druane10 <druane32@yahoo.com>
2017-08-21 18:08:05 -0700

    lazyload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,36 @@
 class PostsController < ApplicationController
+  before_action :get_posts, only: [:all, :funny, :awww, :satisfying, :knowledge, :nature, :sports]
+
   def all
   end
 
   def funny
+    @posts = @posts.select { |post| post.category % 2 == 0 }
   end
 
   def awww
+    @posts = @posts.select { |post| post.category % 3 == 0 }
   end
 
   def satisfying
+    @posts = @posts.select { |post| post.category % 5 == 0 }
   end
 
   def knowledge
+    @posts = @posts.select { |post| post.category % 7 == 0 }
   end
 
   def nature
+    @posts = @posts.select { |post| post.category % 11 == 0 }
   end
 
   def sports
+    @posts = @posts.select { |post| post.category % 13 == 0 }
+  end
+
+  private
+
+  def get_posts
+    @posts = Post.all.order(created_at: :desc)
   end
 end
